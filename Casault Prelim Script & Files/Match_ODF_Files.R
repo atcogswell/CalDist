715745a77af4b622634d16aeacfc606c4791bde8
Andrew Cogswell <andrew.cogswell@dfo-mpo.gc.ca>
2017-01-23 16:29:43 -0400

    Large Modifications
    Changes to script including loop to compile ODF metadata from xls spreadsheets in archive.  The data is then reshaped to fit in original code designed by Benoit to match ctd metadata and plankton metadata.

--- Casault Prelim Script & Files/Match_ODF_Files.R
+++ Casault Prelim Script & Files/Match_ODF_Files.R
@@ -1,4 +1,90 @@
-## Code for matching ODF files
+## Read in and compile CTD excel metadata from ODF files 1978 to 2008 (only run to update .rds file) ####
+library(xlsx)
+library(dplyr)
+library(readxl)
+
+yr<-c(1978:2008)
+
+for (i in 1:length(yr)){
+  
+  setwd(paste("//dcnsbiona01a/BIODataSvcArc/Archive/ctd/",yr[i],sep=""))
+  path<-getwd()
+  xlist<-list.files(pattern="*^.*metadata.*.xls$")
+  out=c(paste("C:/Users/CogswellA/Documents/AZMP/Projects/Vertical distribution CTD project/ODF xl files/",yr[i],"/", sep=""))
+
+for (n in 1:length(xlist)) {
+  
+  dir.create(file.path(out), showWarnings = FALSE)
+  file.copy(from=xlist[n],to=out, overwrite=T,recursive=F)  
+  
+ }
+}
+
+
+##Compile xcel table from local copy and add benoit's data where mission = HUD2008037 - do not run copy if possible - errors in source data ####
+xl<-NULL
+for (i in 1:length(yr)){
+  
+  setwd(paste("C:/Users/CogswellA/Documents/AZMP/Projects/Vertical distribution CTD project/ODF xl files/",yr[i],"/", sep=""))
+  path<-getwd()
+  xlist<-list.files(pattern="*^.*metadata.*.xls$")
+  tmp2<-NULL
+  
+  for (n in 1:length(xlist)){
+    sh<-getSheets(loadWorkbook(xlist[n]))
+    tmp1<-read_excel(xlist[n],sheet = length(sh))
+    tmp1<-as.data.frame(tmp1)
+    if (is.double(tmp1$Cruise_Number==TRUE)) tmp1$Cruise_Number<-as.numeric(tmp1$Cruise_Number)
+    if (is.numeric(tmp1$Cruise_Number==TRUE)) tmp1$Cruise_Number<-as.character(tmp1$Cruise_Number)
+    #tmp1<-read.xlsx(xlist[n],sheetIndex = length(sh),stringsAsFactors=F)
+    tmp1<-subset(tmp1,!is.na(tmp1$File_Spec))
+    tmp1$File_Name<-toupper(tmp1$File_Name)
+    #index1 <- with(tmp1, grepl("DN", tmp1$File_Name)) 
+    #tmp1<-tmp1[index1,] #keeps files only with DN in file name
+    tmp1<-tmp1[c(1,4,13,18,20,21)]
+    #tmp1<-tmp1[!(tmp1$Start_Date_Time== "17-NOV-1858 00:00:00.00"),]
+    #tmp1$Start_Date_Time<-as.POSIXct(tmp1$Start_Date_Time)
+    tmp2<-rbind(tmp2,tmp1)
+    
+  }
+ 
+ tmp2<-dplyr::arrange(tmp2,Cruise_Number) 
+ xl<-rbind(xl,tmp2) 
+  
+}
+
+setwd("C:/Users/CogswellA/Documents/AZMP/Projects/Vertical distribution CTD project/CalDist") 
+xl$Cruise_Number[xl$Cruise_Number=="79031.000000"]<-"79031"
+xl<-subset(xl,xl$Initial_Latitude<=90 & xl$Initial_Latitude>=-90)
+xl<-subset(xl,xl$Initial_Longitude<=180 & xl$Initial_Longitude>=-180)
+saveRDS(xl, "ODF_Meta_1978_2008.rds")
+
+## Read in compiled xls metadata from ODF archive ####
+setwd("C:/Users/CogswellA/Documents/AZMP/Projects/Vertical distribution CTD project/CalDist") 
+xl <- readRDS("ODF_Meta_1978_2008.rds")
+
+xl$YEAR<-strftime(xl$Start_Date_Time,format="%Y")
+xl$TIME <- strftime(xl$Start_Date_Time, format="%H%M",tz="GMT")
+xl$MONTH<-strftime(xl$Start_Date_Time,format="%m")
+xl$DAY<-strftime(xl$Start_Date_Time,format="%d")
+xl<-xl[c(7,9,10,8,5,6,1)]
+names(xl)<-c("YEAR","MONTH","DAY","TIME","LATITUDE","LONGITUDE","ODF_FILENAME")
+
+## Bring in Benoit's PTRAN extraction and subset HUD2008037 because it is not summarized in xcel files in SvcArc
+xlold<-read.table("CTD_Metadata_20161220.tsv",header=TRUE, sep="\t", stringsAsFactors=FALSE, na.strings="", check.names=FALSE, comment.char="")
+xlold$ODF_FILENAME<-paste(xlold$ODF_FILENAME,".ODF",sep="")
+index2 <- with(xlold, grepl("2008037", xlold$ODF_FILENAME)) 
+xlold<-xlold[index2,] #keeps files only with CTD_HUD2008037 in file name
+xl<-rbind(xl,xlold)
+
+
+library(leaflet)
+
+leaflet(xl) %>%
+  fitBounds(-100,12,-8,80) %>%
+addTiles() %>%  # Add default OpenStreetMap map tiles
+addCircles(lng=xl$LONGITUDE, lat=xl$LATITUDE, weight = 3, radius=40, color="red", stroke = TRUE, fillOpacity = 1) 
+## Code for matching ODF files ####
 
 ## load required package
 library(dplyr)
@@ -6,14 +92,16 @@ library(tidyr)
 library(lubridate)
 
 # source custom functions
-source("~/Projects/Utils/R/math_functions/dtr.R")
+source("~/AZMP/Projects/Vertical distribution CTD project/CalDist/Casault Prelim Script & Files/dtr.R")
 
 ## clean memory
 gc()
 
 ##--------------------------------------------------------------------------------------------
 # load ctd metadata
-load("~/Projects/KKrumhansl_Data/outputs/CTD_Metadata_20161220.RData")
+# load("~/AZMP/Projects/Vertical distribution CTD project/CalDist/Casault Prelim Script & Files/CTD_Metadata_20161220.RData")
+
+df_ctd_id<-xl
 
 # add new variables
 df_ctd_id <- df_ctd_id %>%
@@ -35,7 +123,7 @@ df_ctd_id <- df_ctd_id %>%
 
 ##--------------------------------------------------------------------------------------------
 # load plankton data
-ifile <- "~/Projects/KKrumhansl_Data/data/SS_Calspp_VertDistr.txt"
+ifile <- paste("C:/Users/CogswellA/Documents/AZMP/Projects/Vertical distribution CTD project/CalDist/CTD_profiles/SS_BIONESS/SS_Calspp_VertDistr.txt",sep="")
 df_plankton_all <- read.table(ifile, header=TRUE, quote="", sep="\t", stringsAsFactors=FALSE, na.strings="", check.names=FALSE, comment.char="")
 
 # kepp only variables of interest
@@ -159,7 +247,8 @@ df_match <- df_match %>% dplyr::arrange(., pl_year, pl_month, pl_day, pl_heure)
 
 # output to csv file
 ## print to csv file - climatology
-ofile <- "~/Projects/KKrumhansl_Data/outputs/Plankton_CTD_Matches_20161222.csv"
+
+ofile <-paste("C:/Users/CogswellA/Documents/AZMP/Projects/Vertical distribution CTD project/CalDist/CTD_Profiles/SS_BIONESS/Plankton_CTD_Matches_",(as.numeric(format(Sys.Date(), "%Y%m%d"))),".csv",sep="")
 cat("Match_Code", file=ofile, sep="\n", append=FALSE)
 cat("0: no match", file=ofile, sep="\n", append=TRUE)
 cat("1: match year/month/day/lat/lon", file=ofile, sep="\n", append=TRUE)
@@ -169,6 +258,17 @@ cat("4: match year/month and day +/-3d; 3 closest locations", file=ofile, sep="\
 cat("", file=ofile, sep="\n", append=TRUE)
 write.table(df_match, file=ofile, append=TRUE, quote=TRUE, sep=",",
             eol="\n", na="NA", dec=".", row.names=FALSE, col.names=TRUE)
+
+setwd("C:/Users/CogswellA/Documents/AZMP/Projects/Vertical distribution CTD project/CalDist/CTD_Profiles/SS_BIONESS")
+
+
+x1<-read.csv("Plankton_CTD_Matches_20161222.csv",skip=7)
+x2<-subset(x1,x1$ctd_odf_filename!="NA")
+print(paste("The count of unique tow records matched is",length(unique(x2$pl_rec)),sep=" "))
+y1<-read.csv("Plankton_CTD_Matches_20170123.csv",skip=7)
+y2<-subset(y1,y1$ctd_odf_filename!="NA")
+print(paste("The count of unique tow records matched is",length(unique(y2$pl_rec)),sep=" "))
+y3<-dplyr::left_join(y1,x1,by="pl_rec")
 
 
 ##--------------------------------------------------------------------------------------------
